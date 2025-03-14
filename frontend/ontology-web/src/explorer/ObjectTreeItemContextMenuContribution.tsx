b99809223dcd8686d3468eae9ba6ae0ab2af47d0
Laurent Fasani <laurent.fasani@obeo.fr>
2025-03-14 11:34:50 +0100

    Add front-end part and initialize back-end for upload owl file
    Signed-off-by: Laurent Fasani <laurent.fasani@obeo.fr>

--- frontend/ontology-web/src/explorer/ObjectTreeItemContextMenuContribution.tsx
+++ frontend/ontology-web/src/explorer/ObjectTreeItemContextMenuContribution.tsx
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2021, 2024 Obeo.
+ * Copyright (c) 2025 Obeo.
  * This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v2.0
  * which accompanies this distribution, and is available at
@@ -18,8 +18,9 @@ import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
 import MenuItem from '@mui/material/MenuItem';
 import { Fragment, forwardRef, useState } from 'react';
+import { UploadOWLFileModal } from '../upload/upload-owl/UploadOWLFileModal';
 
-type Modal = 'CreateNewObject' | 'CreateNewRepresentation';
+type Modal = 'CreateNewObject' | 'CreateNewRepresentation' | 'ImportOWL';
 
 export const ObjectTreeItemContextMenuContribution = forwardRef(
   (
@@ -30,9 +31,9 @@ export const ObjectTreeItemContextMenuContribution = forwardRef(
     const { setSelection } = useSelection();
 
     console.log(item.kind);
-    // if (!item.kind.startsWith('siriusComponents://semantic')) {
-    //   return null;
-    // }
+    const displayNewObject = !item.kind.startsWith('siriusWeb://document');
+    const displayNewRepresentation = !item.kind.startsWith('siriusWeb://document');
+    const displayImportOWL = item.kind.startsWith('siriusWeb://document');
 
     const onObjectCreated = (selection: Selection) => {
       setSelection(selection);
@@ -59,33 +60,53 @@ export const ObjectTreeItemContextMenuContribution = forwardRef(
           onClose={onClose}
         />
       );
+    } else if (modal === 'ImportOWL') {
+      modalElement = <UploadOWLFileModal editingContextId={editingContextId} onClose={onClose} />;
     }
 
     return (
       <Fragment key="object-tree-item-context-menu-contribution">
-        <MenuItem
-          key="new-object"
-          onClick={() => setModal('CreateNewObject')}
-          data-testid="new-object"
-          disabled={readOnly}
-          ref={ref}
-          aria-disabled>
-          <ListItemIcon>
-            <AddIcon fontSize="small" />
-          </ListItemIcon>
-          <ListItemText primary="New object" />
-        </MenuItem>
-        <MenuItem
-          key="new-representation"
-          onClick={() => setModal('CreateNewRepresentation')}
-          data-testid="new-representation"
-          disabled={readOnly}
-          aria-disabled>
-          <ListItemIcon>
-            <AddIcon fontSize="small" />
-          </ListItemIcon>
-          <ListItemText primary="New representation" />
-        </MenuItem>
+        {displayImportOWL && (
+          <MenuItem
+            key="import-owl"
+            onClick={() => setModal('ImportOWL')}
+            data-testid="import-owl"
+            disabled={readOnly}
+            ref={ref}
+            aria-disabled>
+            <ListItemIcon>
+              <AddIcon fontSize="small" />
+            </ListItemIcon>
+            <ListItemText primary="Upload OWL file" />
+          </MenuItem>
+        )}
+        {displayNewObject && (
+          <MenuItem
+            key="new-object"
+            onClick={() => setModal('CreateNewObject')}
+            data-testid="new-object"
+            disabled={readOnly}
+            ref={ref}
+            aria-disabled>
+            <ListItemIcon>
+              <AddIcon fontSize="small" />
+            </ListItemIcon>
+            <ListItemText primary="New object" />
+          </MenuItem>
+        )}
+        {displayNewRepresentation && (
+          <MenuItem
+            key="new-representation"
+            onClick={() => setModal('CreateNewRepresentation')}
+            data-testid="new-representation"
+            disabled={readOnly}
+            aria-disabled>
+            <ListItemIcon>
+              <AddIcon fontSize="small" />
+            </ListItemIcon>
+            <ListItemText primary="New representation" />
+          </MenuItem>
+        )}
         {modalElement}
       </Fragment>
     );
